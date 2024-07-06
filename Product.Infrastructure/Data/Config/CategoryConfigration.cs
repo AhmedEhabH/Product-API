e424f8f0ee0e1fbcdf880a4246c40e04370740cb
AhmedEhabH <ahmed.ehab.h@gmail.com>
2024-07-06 19:52:31 +0300

    Add project files.

---
+++ Product.Infrastructure/Data/Config/CategoryConfigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Product.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Infrastructure.Data.Config
+{
+    public class CategoryConfigration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(50);
+            builder.Property(x => x.Description).HasMaxLength(50);
+
+            // Seeding
+            builder.HasData(
+                new Category { Id = 1, Name = "Category 1", Description = "Description 1" },
+                new Category { Id = 2, Name = "Category 2", Description = "Description 2" },
+                new Category { Id = 3, Name = "Category 3", Description = "Description 3" }
+                );
+        }
+    }
+}
---
+++ Product.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Product.Core.Interface;
+using Product.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().AsNoTracking().ToList();
+        }
+
+        public IEnumerable<T> GetAll(params Expression<Func<T, bool>>[] includes)
+        {
+            return _context.Set<T>().AsNoTracking().ToList();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, bool>>[] includes)
+        {
+            var query = _context.Set<T>().AsQueryable();
+            foreach (var item in includes)
+            {
+                query = query.Include(item);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>().AsQueryable();
+            foreach (var item in includes)
+            {
+                query = query.Include(item);
+            }
+            return await ((DbSet<T>)query).FindAsync(id);
+        }
+
+        public async Task UpdateAsync(int id, T entity)
+        {
+            var entityValue = await _context.Set<T>().FindAsync(id);
+            if (entityValue is not null)
+            {
+                _context.Update(entityValue);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
